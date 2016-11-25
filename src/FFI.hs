f923c2ee22547a6debf148f8cafec08fa5f766bb
Sibi Prabakaran <sibi@psibi.in>
2016-11-26 01:28:44 +0530

    Add FFI code

--- src/FFI.hs
+++ src/FFI.hs
@@ -1,4 +1,7 @@
 {-#LANGUAGE MagicHash#-}
+{-#LANGUAGE FlexibleContexts#-}
+{-#LANGUAGE TypeFamilies#-}
+{-#LANGUAGE DataKinds#-}
 
 module FFI where
 
@@ -13,3 +16,22 @@ foreign import java unsafe "@static eta.examples.Utils.getClockTimePrim" getCloc
 foreign import java unsafe "@static eta.examples.Utils.getTimeInUTC" getTimeInUTC :: Int64 -> Calendar
 
 foreign import java unsafe "@static eta.examples.Utils.getTZ" getTZ :: Calendar -> JString
+
+foreign import java unsafe "@static eta.examples.Utils.getNames" getNames :: StringArray
+
+type instance Inherits JString = '[Object]
+type instance Inherits StringArray = '[Object]
+
+-- https://docs.oracle.com/javase/7/docs/api/java/lang/reflect/Array.html#getLength(java.lang.Object)
+foreign import java unsafe "@static java.lang.reflect.Array.getLength" getLength :: (Extends a Object) => a -> Int
+
+-- https://docs.oracle.com/javase/7/docs/api/java/lang/reflect/Array.html#newInstance(java.lang.Class,%20int)
+foreign import java unsafe "@static java.lang.reflect.Array.newInstance" newArray :: JClass -> Int -> Object
+
+-- https://docs.oracle.com/javase/7/docs/api/java/lang/reflect/Array.html#get(java.lang.Object,%20int)
+foreign import java unsafe "@static java.lang.reflect.Array.get" arrayGet :: (Extends a Object) => a -> Int -> Object 
+
+conv :: StringArray -> [JString] 
+conv arr = map (unsafeCast . arrayGet arr') [0..len - 1] 
+    where len = getLength arr
+          arr' = newArray (getClass "java/lang/String") len
