1eeba31644f6054dd9b25e3850e3e1da9e572ce9
Sibi Prabakaran <sibi@psibi.in>
2016-12-04 00:20:26 +0530

    Fix null pointer error

--- src/FFI.hs
+++ src/FFI.hs
@@ -17,10 +17,7 @@ foreign import java unsafe "@static eta.examples.Utils.getTimeInUTC" getTimeInUT
 
 foreign import java unsafe "@static eta.examples.Utils.getTZ" getTZ :: Calendar -> JString
 
-foreign import java unsafe "@static eta.examples.Utils.getNames" getNames :: StringArray
-
-type instance Inherits JString = '[Object]
-type instance Inherits StringArray = '[Object]
+foreign import java unsafe "@static eta.examples.Utils.getNames" getNames :: JStringArray
 
 -- https://docs.oracle.com/javase/7/docs/api/java/lang/reflect/Array.html#getLength(java.lang.Object)
 foreign import java unsafe "@static java.lang.reflect.Array.getLength" getLength :: (Extends a Object) => a -> Int
@@ -31,7 +28,7 @@ foreign import java unsafe "@static java.lang.reflect.Array.newInstance" newArra
 -- https://docs.oracle.com/javase/7/docs/api/java/lang/reflect/Array.html#get(java.lang.Object,%20int)
 foreign import java unsafe "@static java.lang.reflect.Array.get" arrayGet :: (Extends a Object) => a -> Int -> Object 
 
-conv :: StringArray -> [JString] 
-conv arr = map (unsafeCast . arrayGet arr') [0..len - 1] 
+conv :: JStringArray -> [JString] 
+conv arr = map (unsafeCast . arrayGet arr) [0..len - 1] 
     where len = getLength arr
           arr' = newArray (getClass "java/lang/String") len
--- src/Main.hs
+++ src/Main.hs
@@ -6,16 +6,10 @@ import GHC.Pack (unpackCString)
 main :: IO ()
 main = do
   print yEAR
-
   let ctime = getClockTimePrim
   print ctime
-
   let cal = getTimeInUTC ctime
       tz = getTZ cal
-           
   print $ unpackCString tz
-        
   let names = getNames
-      print $ map unpackCString (conv names)
-  
-  
+  print $ map unpackCString (conv names)
