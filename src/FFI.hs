3709667a2ff962ddb5ceb0f1034c2541ed22ac7d
Sibi Prabakaran <sibi@psibi.in>
2016-12-05 00:17:21 +0530

    Port if for new eta changes

--- src/FFI.hs
+++ src/FFI.hs
@@ -6,6 +6,7 @@
 module FFI where
 
 import Data.Int (Int64)
+import Data.Proxy
 
 data {-# CLASS "java.util.Calendar" #-} Calendar = Calendar (Object# Calendar)
 
@@ -23,7 +24,7 @@ foreign import java unsafe "@static eta.examples.Utils.getNames" getNames :: JSt
 foreign import java unsafe "@static java.lang.reflect.Array.getLength" getLength :: (Extends a Object) => a -> Int
 
 -- https://docs.oracle.com/javase/7/docs/api/java/lang/reflect/Array.html#newInstance(java.lang.Class,%20int)
-foreign import java unsafe "@static java.lang.reflect.Array.newInstance" newArray :: JClass -> Int -> Object
+foreign import java unsafe "@static java.lang.reflect.Array.newInstance" newArray :: JClass a -> Int -> Object
 
 -- https://docs.oracle.com/javase/7/docs/api/java/lang/reflect/Array.html#get(java.lang.Object,%20int)
 foreign import java unsafe "@static java.lang.reflect.Array.get" arrayGet :: (Extends a Object) => a -> Int -> Object 
@@ -31,7 +32,7 @@ foreign import java unsafe "@static java.lang.reflect.Array.get" arrayGet :: (Ex
 conv :: JStringArray -> [JString] 
 conv arr = map (unsafeCast . arrayGet arr) [0..len - 1] 
     where len = getLength arr
-          arr' = newArray (getClass "java/lang/String") len
+          arr' = newArray (getClass (Proxy :: Proxy JString)) len
 
 
 -- Enum Example
@@ -53,3 +54,14 @@ instance Class (JEnum a) where
 type instance Inherits PosixFilePermission = '[JEnum PosixFilePermission]
 
 foreign import java unsafe name :: (Extends a (JEnum a)) => a -> JString
+
+-- Set Example 
+
+data {-# CLASS "java.nio.file.Paths" #-} Paths = Paths (Object# Paths) 
+data {-# CLASS "java.nio.file.Path" #-} Path = Path (Object# Path) 
+data {-# CLASS "java.util.Set" #-} JSet a = JSet (Object# (JSet a))
+
+foreign import java unsafe "@static java.nio.file.Paths.get" get :: JString -> Path
+
+-- http://docs.oracle.com/javase/7/docs/api/java/nio/file/Files.html#setPosixFilePermissions%28java.nio.file.Path,%20java.util.Set%29
+foreign import java unsafe "@static java.nio.file.Files.setPosixFilePermissions" setPosixFilePermissions :: JString -> Path -> IO Path
